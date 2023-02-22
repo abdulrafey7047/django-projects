b87f4ea12d1c41bcfc194e7e7d3bce8d781a3639
Abdul Rafey <abdulrafey@Rafeys-MacBook-Pro.local>
2023-02-22 21:36:38 +0500

    Addded server-setup shell scripts

---
+++ server-setup/install-and-run-docker.sh
@@ -0,0 +1,14 @@
+echo "INSTALLING DOCKER"
+
+sudo yum update -y
+
+sudo amazon-linux-extras install docker
+
+sudo service docker start
+
+sudo usermod -a -G docker ec2-user
+
+sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+
+sudo chmod +x /usr/local/bin/docker-compose
+
--- server-setup/install-docker.sh
+++
@@ -1,1 +0,0 @@
-echo "INSTALLING DOCKER"
