ad052566533f589b3e6a8c7ef9251729eaa5fc84
Abdul Rafey <abdulrafey@Rafeys-MacBook-Pro.local>
2023-02-22 21:40:51 +0500

    Updated server-setup/install-and-run-docker.sh

--- server-setup/install-and-run-docker.sh
+++ server-setup/install-and-run-docker.sh
@@ -11,4 +11,5 @@ sudo usermod -a -G docker ec2-user
 sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 
 sudo chmod +x /usr/local/bin/docker-compose
+sudo chmod 666 /var/run/docker.sock
 
